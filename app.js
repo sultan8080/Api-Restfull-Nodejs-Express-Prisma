2092fbafd822379f679bd23b30f4687443c8de8d
Moti-Sultan <motisultannur@gmail.com>
2025-04-30 09:21:27 +0200

    Product API POST ET GET créé sur app.js

--- app.js
+++ app.js
@@ -23,7 +23,32 @@ app.use(
 
 // créer un nouveau produit POST
 
+app.post("/api/products", async (req, res) => {
+    try {
+      const { name, description, price, inStock } = req.body;
+  
+      const newProduct = await prisma.product.create({
+        data: { name, description, price, inStock },
+      });
+  
+      res.status(201).json({
+        message: "Objet créé !",
+        thing: newThing,
+      });
+    } catch (error) {
+      res.status(500).json({ error: "Erreur lors de la création", details: error });
+    }
+  });
 
+// Afficher tous les produits GET
+app.get("/api/products", async (req, res) => {
+    try {
+      const product = await prisma.product.findMany();
+      res.status(200).json(stuff);
+    } catch (error) {
+      res.status(500).json({ error: "Erreur lors de la récupération", details: error });
+    }
+  });
 
 // Facultatif : fermer la connexion Prisma correctement
   process.on("SIGINT", async () => {
