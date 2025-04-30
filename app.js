a4b8e6ebb3fcb60da86eb9cd6cd4fdb3896349e9
Moti-Sultan <motisultannur@gmail.com>
2025-04-30 09:20:31 +0200

    Prisma Schema Model créé pour produit

--- app.js
+++ app.js
@@ -1,7 +1,34 @@
-const express = require('express');
+const express = require("express");
+const { PrismaClient } = require("./generated/prisma");
+const cors = require("cors");
 
 const app = express();
+const prisma = new PrismaClient();
+
+app.use(
+  cors({
+    origin: "http://localhost:4200", // Autorise uniquement Angular
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: [
+      "Origin",
+      "X-Requested-With",
+      "Content",
+      "Accept",
+      "Content-Type",
+      "Authorization",
+    ],
+  })
+);
 
 
+// créer un nouveau produit POST
+
+
+
+// Facultatif : fermer la connexion Prisma correctement
+  process.on("SIGINT", async () => {
+    await prisma.$disconnect();
+    process.exit(0);
+  });
 
 module.exports = app;
